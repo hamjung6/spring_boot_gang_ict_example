e8d929590f957f2e3ba9f62bec7cfbaebb667437
WIN-51BV20CU4GG\ICT02_16 <hamjunblack6@gmail.com>
2024-09-27 11:38:47 +0900

    스프링 시큐리티 5

--- src/main/java/edu/ict/ex/config/SecurityConfig.java
+++ src/main/java/edu/ict/ex/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package edu.ict.ex.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
 //import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -12,46 +15,65 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import edu.ict.ex.security.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-		// web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
-	}
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    
+   @Override
+   public void configure(WebSecurity web) throws Exception {
+      // web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+      //web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+   }
+   
+   //권한 설정
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+        //우선 CSRF설정을 해제한다.
+        //초기 개발시만 해주는게 좋다.
+        http.csrf(csrf -> csrf.disable());
+      
+      http.authorizeHttpRequests()
+      .antMatchers("/board/**").hasAnyRole("USER") 
+      .antMatchers("/boards/**").hasAnyRole("ADMIN")
+      .antMatchers("/**").permitAll();
 
-	// 권한 설정
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// 우선 CSRF설정을 해제한다.
-		// 초기 개발시만 해주는게 좋다.
-		http.csrf(csrf -> csrf.disable());
-
-		http.authorizeHttpRequests()
-		.antMatchers("/board/**").hasAnyRole("USER")
-		.antMatchers("/boards/**").hasAnyRole("ADMIN")
-		.antMatchers("/**").permitAll();
-
-		// 로그인 폼 커스텀 마이징
-		http.formLogin() // 스프링 시큐리티에 있는 기본 로그인 폼을 사용하겠다.
-			.loginPage("/login") // loginPage()는 말 그대로 로그인 할 페이지 url이고
-			.usernameParameter("id")
-			.passwordParameter("pw")
-			.defaultSuccessUrl("/");
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		auth.inMemoryAuthentication()
-		.withUser("user").password("{noop}user").roles("USER")
-		.and()
-		.withUser("admin").password("{noop}admin").roles("ADMIN");
-
-	}
+      //로그인 폼 커스텀 마이징
+        http.formLogin()
+           .loginPage("/login")  //loginPage() 는 말그대로 로그인할 페이지 url 이고
+            .usernameParameter("id")
+            .passwordParameter("pw")
+            .defaultSuccessUrl("/");           
+      
+   }
+   
+   
+   @Override
+   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       
+//      auth.inMemoryAuthentication()
+//           .withUser("user").password("{noop}user").roles("USER").
+//           and()
+//           .withUser("admin").password("{noop}admin").roles("ADMIN");
+      
+      auth.userDetailsService(customUserDetailsService)
+            .passwordEncoder(passwordEncoder());
+   
+   }
+   
+   @Bean
+    public PasswordEncoder passwordEncoder() {        
+      return new BCryptPasswordEncoder();
+    }
+      
+   
 
 //   @Override
 //   protected void configure(HttpSecurity http) throws Exception {
@@ -80,5 +102,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //       public void configure(AuthenticationManagerBuilder auth) throws Exception {
 //           //auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
 //       }
-
+   
 }
--- src/test/java/edu/ict/ex/mapper/UserMapperTest.java
+++ src/test/java/edu/ict/ex/mapper/UserMapperTest.java
@@ -1,10 +1,13 @@
 package edu.ict.ex.mapper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edu.ict.ex.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
@@ -13,20 +16,54 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootTest
 class UserMapperTest {
 
-	@Autowired
-	private UserMapper userMapper;
+   @Autowired
+   private UserMapper userMapper;
 
-	@Test
-	void testUserMapper() {
-		assertNotNull(userMapper);
-	}
+   @Test
+   void testUserMapper() {
+      assertNotNull(userMapper);
+   }
 
-	@Test
-	void testGetUser() {
-		UserVO user = userMapper.getUser("admin");
-		assertNotNull(user);
-		System.out.println(user);
+   @Test
+   void testGetUser() {
+      UserVO user = userMapper.getUser("admin");
+      assertNotNull(user);
 
-	}
+      System.out.println(user);
 
+   }
+
+   // 단방향(MD5, SHA 등) : 암호화(encoding)는 가능해도 복보화(decoding)는 안 되는 것
+   // 양방향(AES, RSA, DES 등) : 암호화 및 복호화가 가능한 것
+
+   @Autowired
+   private PasswordEncoder passwordEncoder;
+   
+//   @Disabled
+   @Test
+   void testInsertUser() {
+
+      UserVO user = new UserVO();
+      user.setUsername("user2");
+      user.setPassword(passwordEncoder.encode("user2"));
+      user.setEnabled("1");
+
+      userMapper.insertUser(user);
+      userMapper.insertAuthorities(user);
+
+      assertNotNull(user);
+
+      System.out.println(user);
+   }
+
+   @Test
+   void testMatcher() {
+
+      UserVO user = userMapper.getUser("admin2");
+      // $2a$10$sAseHf9twiHZZkgm0HsWuutwUymCreWR2DOWUh3qHbf4rngU5X4P6
+      boolean isMatch = passwordEncoder.matches("admin2", user.getPassword());
+
+      System.out.println(user.getPassword()); 
+      assertEquals(isMatch, true);
+   }
 }
